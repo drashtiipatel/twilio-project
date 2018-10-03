a31932ccf9d6d4ee7ccfac53e8425afd8af1821c
Drashti Patel <43542968+drashtiipatel@users.noreply.github.com>
2018-10-03 18:26:14 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+ 
+// Update the path below to your autoload.php, 
+// see https://getcomposer.org/doc/01-basic-usage.md 
+require_once "vendor/autoload.php"; 
+ 
+use Twilio\Rest\Client; 
+ echo "You will get msg";
+$sid    = "ACa2ae59874300045d810a55cc1a10c7f3"; 
+$token  = "e58e45bc04a0b36c2bd251e577419c11"; 
+$twilio = new Client($sid, $token); 
+
+$message = $twilio->messages 
+                  ->create("+18622239425", // to 
+                           array( 
+                               "from" => "+19733520445",       
+                               "body" => "hello Drashti CS 643 Fall 2018 " 
+                           ) 
+                  ); 
+ 
